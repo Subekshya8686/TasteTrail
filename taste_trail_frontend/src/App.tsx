335ef2ca627c22c31280bf647db1d9a2eb521dd8
Purnima8555 <110591365+Purnima8555@users.noreply.github.com>
2024-01-03 21:33:15 +0545

    updated category

--- taste_trail_frontend/src/App.tsx
+++ taste_trail_frontend/src/App.tsx
@@ -7,7 +7,6 @@ import Upload from "./pages1/upload.tsx";
 import LoginRegister from "./pages1/LoginRegister.tsx";
 import Category_snacks from "./pages1/Category_snacks.tsx";
 import Recipes from "./pages1/Recipes.tsx";
-// import React from "react";
 import UserProfile from "./pages1/userProfile.tsx";
 import Category_snacks1 from "./pages1/Category_snacks1.tsx";
 
