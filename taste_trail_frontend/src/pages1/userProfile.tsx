86efe4aa2d79ba41e4c64ae6bada78c9afc6332a
Pramudita29 <pramuditabhattarai25@gmail.com>
2024-01-04 07:04:13 +0545

    Profile

--- taste_trail_frontend/src/pages1/userProfile.tsx
+++ taste_trail_frontend/src/pages1/userProfile.tsx
@@ -151,7 +151,6 @@ const UserProfile: React.FC<UserProfileProps> = ({ userId }) => {
                     </div>
                 </div>
             </div>
-            <Footer />
         </>
     );
 };
