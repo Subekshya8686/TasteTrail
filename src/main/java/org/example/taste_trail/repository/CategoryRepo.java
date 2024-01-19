6021b32746244cfd9e50d19deadfa980a01d432e
Pramudita29 <pbhattarai0129@gmail.com>
2024-01-19 12:35:31 +0545

    updated

--- src/main/java/org/example/taste_trail/repository/CategoryRepo.java
+++
@@ -1,7 +0,0 @@
-package org.example.taste_trail.repository;
-
-import org.example.taste_trail.entity.CategoryEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepo extends JpaRepository<CategoryEntity, Integer> {
-}
